8a9ab6180841d09e5422e4a45363b20ae83ebca1
yohanei <sleepcat10235269@gmail.com>
2018-05-02 11:04:51 +0800

    11:04

--- sw.js
+++ sw.js
@@ -1,2 +1,12 @@
-¡yuse strict¡z;
-importScripts(¡ysw-toolbox.js¡z); toolbox.precache([¡uindex.html¡v,¡vstyle/style.css¡v]); toolbox.router.get(¡y/images/*¡z, toolbox.cacheFirst); toolbox.router.get(¡y/*¡z, toolbox.networkFirst, { networkTimeoutSeconds: 5});
+
+'use strict';
+
+importScripts('sw-toolbox.js');
+
+toolbox.precache(["index.html","style/style.css"]);
+
+toolbox.router.get('/images/*', toolbox.cacheFirst);
+
+toolbox.router.get('/*', toolbox.networkFirst, {
+  networkTimeoutSeconds: 5
+});
