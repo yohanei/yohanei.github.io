31bfc3ce64cdecfefeba3e518ef6ae355f188efa
yohanei <sleepcat10235269@gmail.com>
2018-05-02 11:01:34 +0800

    11:01

--- sw.js
+++ sw.js
@@ -1,27 +1,2 @@
-    if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }).catch(function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-var CACHE_NAME = "my_cache";
-   var urlsToCache = [
-    '/index.html',
-    '/css/style.css',
-    '/js/script.js'
-   ];
-
-self.addEventListener('install', function(event) {
-
-event.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache) {
-             console.log('Opendhe : ',cache);
-            return cache.addAll(urlsToCache);
-      })
-    );
-});
+¡yuse strict¡z;
+importScripts(¡ysw-toolbox.js¡z); toolbox.precache([¡uindex.html¡v,¡vstyle/style.css¡v]); toolbox.router.get(¡y/images/*¡z, toolbox.cacheFirst); toolbox.router.get(¡y/*¡z, toolbox.networkFirst, { networkTimeoutSeconds: 5});
